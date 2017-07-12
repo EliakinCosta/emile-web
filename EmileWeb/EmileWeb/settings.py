d7754013d81e26104ed842b9b8ccf0062d1971a2
Eliakin Costa <eliakim170@gmail.com>
2017-07-12 16:30:22 -0300

    Criar configuracao de arquivos estaticos

--- EmileWeb/EmileWeb/settings.py
+++ EmileWeb/EmileWeb/settings.py
@@ -135,3 +135,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
