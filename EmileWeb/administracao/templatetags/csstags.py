e817d5d71cc1fd4d3df7f8f02ad1aa3de6f8278c
EliakinCosta <eliakim170@gmail.com>
2017-07-13 18:12:45 -0300

    Formatar Mural de mensagens

--- EmileWeb/administracao/templatetags/csstags.py
+++ EmileWeb/administracao/templatetags/csstags.py
@@ -1,4 +1,5 @@
 from django import template
+import datetime
 register = template.Library()
 
 
@@ -10,3 +11,12 @@ def addcss(field, css):
 @register.filter(is_safe=True)
 def label_with_classes(value, arg):
     return value.label_tag(attrs={'class': arg})
+
+
+@register.filter(name='print_timestamp')
+def print_timestamp(timestamp):
+    try:
+        ts = int(timestamp)
+    except ValueError:
+        return None
+    return datetime.datetime.fromtimestamp(ts)
