3ac58c0368e53085f7b2515235aed86d8f318d76
EliakinCosta <eliakim170@gmail.com>
2017-07-13 18:01:22 -0300

    Implementar mural de mensagens

--- EmileWeb/administracao/views.py
+++ EmileWeb/administracao/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import requests
 
 
 # Create your views here.
@@ -8,4 +9,9 @@ def home(request):
 
 
 def wall_messages_list(request):
+    response = requests.get('http://emile-server.herokuapp.com/wall_messages/{0}'.format(request.user.email))
+
+    if response.status_code == 200:
+        _json = response.json()        
+        return render(request, 'administracao/wall_messages_list.html', _json)
     return render(request, 'administracao/wall_messages_list.html', {})
