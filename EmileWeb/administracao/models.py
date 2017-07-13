58b7e07c9010d5a27badc8f2941eed4943d39a95
EliakinCosta <eliakim170@gmail.com>
2017-07-13 18:30:49 -0300

    Implementar modelo que representa a imagem da mensagem

--- EmileWeb/administracao/models.py
+++ EmileWeb/administracao/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class MessageFile(models.Model):
+    message_file = models.FileField(upload_to='wall_messages_files/')
+    message_id = models.IntegerField(unique=True)
