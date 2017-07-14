f8d687b785c30bd8a0bc4e6e1dfbc8b71c1b5907
Eliakin Costa <eliakim170@gmail.com>
2017-07-14 00:03:01 -0300

    Incluindo login required

--- EmileWeb/administracao/views.py
+++ EmileWeb/administracao/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render
 import requests
+from django.contrib.auth.decorators import login_required
 
 
-# Create your views here.
-
+@login_required
 def home(request):
     return render(request, 'administracao/base.html', {})
 
 
+@login_required
 def wall_messages_list(request):
     response = requests.get('http://emile-server.herokuapp.com/wall_messages/{0}'.format(request.user.email))
 
     if response.status_code == 200:
-        _json = response.json()        
+        _json = response.json()
         return render(request, 'administracao/wall_messages_list.html', _json)
     return render(request, 'administracao/wall_messages_list.html', {})
