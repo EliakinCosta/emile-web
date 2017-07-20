03ad209b36543c642996991092be4449d44d84fb
Eliakin Costa <eliakim170@gmail.com>
2017-07-20 17:53:33 -0300

    Implementacao do django select no template

--- EmileWeb/administracao/forms.py
+++ EmileWeb/administracao/forms.py
@@ -1,14 +1,23 @@
 from django import forms
 from administracao import models
+from django_select2.forms import Select2MultipleWidget
 
 
 def get_institutions():
     return [('', '-----'), (1, 'IFBA')]
 
 
+NUMBER_CHOICES = [
+    (1, 'One'),
+    (2, 'Two'),
+    (3, 'Three'),
+    (4, 'Four'),
+]
+
+
 class MessageForm(forms.ModelForm):
     user_type_destination = forms.ChoiceField(choices=[], label='Tipo de destino', required=True)
-    course_section = forms.ChoiceField(choices=[], label='Turma', required=False)
+    course_section = forms.MultipleChoiceField(widget=Select2MultipleWidget(attrs={'data-language': 'pt-BR','placeholder': 'Escolha um Operador'}), choices=NUMBER_CHOICES)
     message = forms.CharField(required=True, label='Mensagem', max_length=250, widget=forms.Textarea)
 
     class Meta:
