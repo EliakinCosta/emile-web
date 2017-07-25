f5f81440fd1d9408d80e283aeeb4907fec6971ec
Eliakin Costa <eliakim170@gmail.com>
2017-07-25 19:14:40 -0300

    Implementar UpdateView e referencia na wall_message_list

--- EmileWeb/administracao/urls.py
+++ EmileWeb/administracao/urls.py
@@ -8,11 +8,12 @@ from django.contrib.auth import views as auth_views
 app_name = 'administracao'
 
 urlpatterns = [
-    url(r'^home/$', views.home, name='home'),
+    url(r'^$', views.home, name='home'),
     url(r'^wall_messages_list/$', views.wall_messages_list, name='wall_messages_list'),
     url(r'^login/$', auth_views.login, {'template_name': 'administracao/login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^wall_message_create/$', views.WallMessageCreateView.as_view(), name='wall_message_create'),
     url(r'^destinations_by_user_type/(?P<pk>\d+)$', views.destinations_by_user_type, name='destinations_by_user_type'),
     url(r'^param_values_service/', views.param_values_service, name='param_values_service'),
+     url('^wall_message/update/(?P<pk>[\w-]+)$', views.FileMessageUpdateView.as_view(), name='wall_message_update'),
 ]
--- EmileWeb/administracao/views.py
+++ EmileWeb/administracao/views.py
@@ -3,6 +3,7 @@ import requests
 from django.contrib.auth.decorators import login_required
 from administracao import forms
 from django.views import View
+from django.views.generic.edit import UpdateView
 from django.core.urlresolvers import reverse_lazy
 import json
 from django.http import JsonResponse
@@ -21,7 +22,15 @@ def home(request):
 @login_required
 def wall_messages_list(request):
     url = '{0}/wall_messages/{1}'.format(settings.BASE_URL, request.user.email)
-    return render(request, 'administracao/wall_messages_list.html', response_to_dict(url))
+    result = response_to_dict(url)
+    filtered_result = []
+
+    for message in result.get('results'):
+        message.update({'has_file': False})
+        if has_file(message):
+            message.update({'has_file': True})
+        filtered_result.append(message)
+    return render(request, 'administracao/wall_messages_list.html', result)
 
 
 def destinations_by_user_type(request, pk):
@@ -37,6 +46,8 @@ def response_to_dict(url):
     return {}
 
 
+def has_file(message):
+    return models.Message.objects.filter(message_id=message.get('id')).all()
 
 def param_values_service(request):
     if request.method == 'POST':
@@ -110,3 +121,14 @@ class WallMessageCreateView(View):
             except OSError as exc: # Guard against race condition
                 if exc.errno != errno.EEXIST:
                     raise
+
+
+class FileMessageUpdateView(UpdateView):
+    model = models.Message
+    fields = ['message_file']
+    template_name = 'administracao/wall_message_update.html'
+
+    def get_object(self, queryset=None):
+        print(self.kwargs['pk'])
+        obj = models.Message.objects.get(message_id=self.kwargs['pk'])
+        return obj
